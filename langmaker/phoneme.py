3db0c8dbe50fae09825241fe562be4e41a3dcace
Mouse Reeve <mousereeve@riseup.net>
2016-06-01 16:34:39 -0700

    Phoneme tests and some fixes
    tests, man. who knew

--- langmaker/phoneme.py
+++ langmaker/phoneme.py
@@ -7,6 +7,9 @@ class Phoneme(object):
 
     def __init__(self, consonants=None, vowels=None):
         # should be comprehensive, or pulled from a transcription class
+        self.consonants = None
+        self.vowels = None
+
         self.phoneme_set = {
             'vowels': ['a', 'e', 'i', 'o', 'u', 'y'],
             'consonants': ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r',
@@ -17,13 +20,13 @@ class Phoneme(object):
 
     def generate_consonants(self):
         ''' generates a selection of consonant phonemes '''
-        # TODO: select a  meaningful phoneme set
-        return self.phoneme_set['consonants']
+        # TODO: select a meaningful phoneme set
+        return self.consonants or self.phoneme_set['consonants']
 
     def generate_vowels(self):
         ''' generates a selection of vowel phonemes '''
-        # TODO: select a  meaningful phoneme set
-        return self.phoneme_set['vowels']
+        # TODO: select a meaningful phoneme set
+        return self.vowels or self.phoneme_set['vowels']
 
     def get_phonemes(self):
         ''' return a complete list of phonemes '''
---
+++ tests/phoneme_tests.py
@@ -0,0 +1,43 @@
+''' test phoneme generator '''
+import unittest
+from unittest import TestCase
+
+from langmaker.phoneme import Phoneme
+
+specified = Phoneme(consonants=['p'], vowels=['o'])
+unspecified = Phoneme()
+
+
+class PhonemeTest(TestCase):
+    ''' phoneme tests '''
+
+    def test_generate_specified(self):
+        ''' tests fetching consonants '''
+        consonants = specified.generate_consonants()
+        self.assertEqual(consonants, ['p'])
+
+        vowels = specified.generate_vowels()
+        self.assertEqual(vowels, ['o'])
+
+    def test_generate_unspecified(self):
+        ''' test automatic phoneme production '''
+        consonants = unspecified.generate_consonants()
+        self.assertTrue(consonants <= unspecified.phoneme_set['consonants'])
+
+        vowels = unspecified.generate_vowels()
+        self.assertTrue(vowels <= unspecified.phoneme_set['vowels'])
+
+    def test_get_consonant(self):
+        ''' retrieve a consonant '''
+        consonant = specified.get_consonant()
+        self.assertEqual(consonant, 'p')
+
+    def test_get_vowel(self):
+        ''' retrieve a vowel '''
+        vowel = specified.get_vowel()
+        self.assertEqual(vowel, 'o')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
