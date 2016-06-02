77f15fb3a002b2fc1b09962132460373abee10ad
Mouse Reeve <mousereeve@riseup.net>
2016-06-01 18:21:44 -0700

    CFG for syllables

--- langmaker/syllable.py
+++ langmaker/syllable.py
@@ -1,17 +1,57 @@
 ''' Generate linguistically consistent syllables '''
+import random
+import re
+from nltk import CFG
+from nltk.parse.generate import generate
+
 from langmaker.phoneme import Phoneme
 
 class Syllable(object):
     ''' combine phonemes into syllables '''
 
-    def __init__(self, phonemes=None):
+    def __init__(self, phonemes=None, onset=None, coda=None):
         self.phonemes = phonemes or Phoneme()
+
+        # use CFG to structure syllables
+        if onset == None: # optional onset
+            onset = 'C | CC | \' \''
+        elif onset: # mandatory onset
+            onset = 'C | CC'
+        else: # no onset
+            onset = '\' \''
+
+        if coda == None: # optional coda
+            coda = 'C | \' \''
+        elif coda: # mandatory coda
+            coda = 'C'
+        else: # no coda
+            coda = '\' \''
+
+        # nucleus is always present
+
+        delimiter = '\' | \''
+        grammar = '''
+        S -> O V K
+        O -> %s
+        K -> %s
+        C -> \'%s\'
+        CC -> \'%s\'
+        V -> \'%s\'
+        ''' % (onset, coda,
+               delimiter.join(self.phonemes.consonants),
+               delimiter.join(self.phonemes.consonant_clusters),
+               delimiter.join(self.phonemes.vowels))
+        self.grammar = CFG.fromstring(grammar)
+        self.syllables = self.generate_syllables()
+
+    def generate_syllables(self):
+        ''' every possible syllable for the given phonemes and grammar '''
+        # spaces, which are only there for NLTK's sake, are removed
+        return [re.sub(' ', '', ''.join(s)) for s in generate(self.grammar, depth=4)]
 
     def get_syllable(self):
         ''' create a syllable '''
-        # TODO: syllable structure assumes cv
-        # should probs use CFG
-        return self.phonemes.get_consonant() + self.phonemes.get_vowel()
+        return random.choice(self.syllables)
 
 if __name__ == '__main__':
     builder = Syllable()
--- tests/syllable_tests.py
+++ tests/syllable_tests.py
@@ -1,23 +1,49 @@
 ''' test syllable generator '''
 import unittest
 from unittest import TestCase
-from unittest.mock import MagicMock
 
 from langmaker.syllable import Syllable, Phoneme
 
-syllableMock = Phoneme
-syllableMock.get_consonant = MagicMock(return_value='p')
-syllableMock.get_vowel = MagicMock(return_value='o')
-instance = Syllable(syllableMock)
+phonemeMock = Phoneme
+phonemeMock.consonants = ['p']
+phonemeMock.consonant_clusters = ['pr']
+phonemeMock.vowels = ['o']
 
 
 class SyllableTest(TestCase):
     ''' syllable tests '''
 
-    def test_given_structure(self):
-        ''' tests specifying the length of a syllable '''
-        # TODO: the code isn't in a state for this yet
-        pass
+    def test_default_grammar(self):
+        ''' NLTK generated syllables '''
+        instance = Syllable(phonemes=phonemeMock)
+        syllables = instance.syllables
+        self.assertEqual(syllables, ['pop', 'po', 'prop', 'pro', 'op', 'o'])
+
+    def test_onset_states(self):
+        ''' NLTK generated syllables with onset flag'''
+        instance = Syllable(phonemes=phonemeMock, onset=True)
+        syllables = instance.syllables
+        self.assertEqual(syllables, ['pop', 'po', 'prop', 'pro'])
+
+        instance = Syllable(phonemes=phonemeMock, onset=False)
+        syllables = instance.syllables
+        self.assertEqual(syllables, ['op', 'o'])
+
+    def test_coda_states(self):
+        ''' NLTK generated syllables with coda flag'''
+        instance = Syllable(phonemes=phonemeMock, coda=True)
+        syllables = instance.syllables
+        self.assertEqual(syllables, ['pop', 'prop', 'op'])
+
+        instance = Syllable(phonemes=phonemeMock, coda=False)
+        syllables = instance.syllables
+        self.assertEqual(syllables, ['po', 'pro', 'o'])
+
+    def test_get_syllable(self):
+        ''' pick a syllablle '''
+        instance = Syllable(phonemes=phonemeMock)
+        syllable = instance.get_syllable()
+        self.assertTrue(syllable in ['pop', 'po', 'prop', 'pro', 'op', 'o'])
 
 
 if __name__ == '__main__':
