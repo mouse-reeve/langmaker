5bacb5b4582554a2816758d3a447079cb118d3f7
Mouse Reeve <mousereeve@riseup.net>
2016-06-01 21:39:08 -0700

    Updates example

--- example.py
+++ example.py
@@ -3,11 +3,18 @@ from langmaker.phoneme import Phoneme
 from langmaker.syllable import Syllable
 from langmaker.morpheme import Morpheme
 from langmaker.word import Word
+from langmaker.grapheme import Grapheme
 
-phoneme = Phoneme(consonants=['b', 'p'], vowels=['o', 'oo'], consonant_clusters=['bl', 'pl', 'pr'])
+consonants = ['b', 'p', 'l']
+vowels = ['o', 'oo']
+
+phoneme = Phoneme(consonants=consonants, vowels=vowels, consonant_clusters=['b/l', 'p/l'])
 syllable = Syllable(phonemes=phoneme, coda=True)
 morpheme = Morpheme(syllables=syllable)
 word = Word(morphemes=morpheme)
+conversions = {p: p for p in consonants + vowels}
+print(conversions)
+grapheme = Grapheme(phonemes=phoneme, words=word, conversions=conversions)
 
 for _ in range(10):
-    print(word.get_word())
+    print(grapheme.write_word())
