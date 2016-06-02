947784aa400873a4de55893e0cee68cc33eb4eac
Mouse Reeve <mousereeve@riseup.net>
2016-06-01 21:20:00 -0700

    Graphemes

--- langmaker/grapheme.py
+++ langmaker/grapheme.py
@@ -1,11 +1,35 @@
 ''' Translate phonemes into graphemes '''
+import json
+import random
+from langmaker import DATA
 from langmaker.phoneme import Phoneme
+from langmaker.word import Word
 
 class Grapheme(object):
     ''' produce graphemes for words '''
 
-    def __init__(self, phonemes=None, conversions=None):
+    def __init__(self, phonemes=None, conversions=None, words=None):
         if phonemes and not conversions:
             raise ValueError('Conversions must be provided with phonemes')
+        self.words = words or Word()
         self.phonemes = phonemes or Phoneme()
-        self.conversions = conversions or []
+
+        data = json.loads(open('%s/graphemes.json' % DATA).read())
+        self.conversions = conversions or data['conversions']
+
+    def write_word(self, word=None):
+        ''' convert phonemes into graphemes '''
+        word = word or self.words.get_word()
+        graphemes = []
+        for phoneme in word.split('/'):
+            try:
+                options = self.conversions[phoneme]
+                graphemes.append(random.choice(options))
+            except KeyError:
+                graphemes.append(phoneme)
+        return ''.join(graphemes)
+
+if __name__ == '__main__':
+    builder = Grapheme()
+    for _ in range(10):
+        print(builder.write_word())
