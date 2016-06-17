4da59590990baa4c3e03c3a34ac264db4a025f42
Mouse Reeve <mousereeve@riseup.net>
2016-06-17 16:06:12 -0700

    Produce a dictionary

--- langmaker/lexeme.py
+++ langmaker/lexeme.py
@@ -1,8 +1,10 @@
-''' System for generating lexemes, lemmas, and words, buildin on existing linguistic data '''
+''' System for generating lexemes, lemmas, and words, building on existing
+linguistic data. Come here for a dictionary '''
 from langmaker.morphology import Morphology
 
 class Lexeme(object):
     ''' works with words '''
+    dictionary = {}
 
     def __init__(self, morphology=None):
         self.morphology = morphology or Morphology()
@@ -10,7 +12,6 @@ class Lexeme(object):
     def get_lemma(self):
         ''' get the dictionary form of a lexeme '''
         return self.morphology.generate_lemma()
-        return self.get_word()
 
     def get_lexeme_set(self):
         ''' produce the words for a lexeme (ie: ['run', 'ran']) '''
@@ -22,7 +23,13 @@ class Lexeme(object):
         # TODO: do some actual morphology
         return self.get_lemma()
 
+    def translate(self, english_word):
+        ''' lookup or create a translation for an english word '''
+        if not english_word in self.dictionary:
+            self.dictionary[english_word] = self.get_lemma()
+        return self.dictionary[english_word]
+
 if __name__ == '__main__':
     builder = Lexeme()
-    for _ in range(10):
-        print(builder.get_lexeme())
+    for word in ['cat', 'flower', 'hate', 'blue', 'sprint', 'quickly']:
+        print('%s: %s' % (word, builder.translate(word)))
