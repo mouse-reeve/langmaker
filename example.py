41cda9ef2e4799792ef9973b9921df3ec914ad98
Mouse Reeve <mousereeve@riseup.net>
2016-06-17 17:15:33 -0700

    Better example script

--- example.py
+++ example.py
@@ -6,16 +6,27 @@ from langmaker.morphology import Morphology
 from langmaker.lexeme import Lexeme
 from langmaker.grapheme import Grapheme
 
-consonants = ['b', 'p', 'l']
-vowels = ['o', 'oo']
+consonants = ['b', 'p', 'l', 'f']
+vowels = ['o', 'oo', 'ou', 'eie']
 
-phoneme = Phoneme(consonants=consonants, vowels=vowels, consonant_clusters=['b/l', 'p/l'])
+phoneme = Phoneme(consonants=consonants, vowels=vowels,
+                  consonant_clusters=['b/l', 'p/l', 'f/l'])
 syllable = Syllable(phonemes=phoneme, coda=True)
 morpheme = Morpheme(syllables=syllable)
 morphology = Morphology(morphemes=morpheme)
 lexeme = Lexeme(morphology=morphology)
-conversions = {p: p for p in consonants + vowels}
+
+conversions = {p: [p] for p in consonants + vowels}
 grapheme = Grapheme(phonemes=phoneme, lexemes=lexeme, conversions=conversions)
 
-for _ in range(10):
-    print(grapheme.write_word())
+translate = [
+    ('cat', 'NN'),
+    ('flower', 'NN'),
+    ('hate', 'VB'),
+    ('blue', 'JJ'),
+    ('sprint', 'VB'),
+    ('quickly', 'RB')
+]
+for word in translate:
+    translated = lexeme.translate(word[0], word[1])
+    print(grapheme.write_word(translated))
